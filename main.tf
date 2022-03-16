9bb059943fd45073dda4d53757c1a01ae24cf552
vicrlda <abreuvictorlima@gmail.com>
2022-03-16 14:25:51 -0300

    @vicrlda POST https://tinyurl.com/t4usxy3a

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-045137e8d34668746"
   instance_type = "t2.micro"
 
   tags = {

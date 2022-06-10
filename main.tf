6fddc9615d8cec6aae4583a32db43c20a5d991fd
Victor Lima Abreu <abreuvictorlima@gmail.com>
2022-06-09 23:44:12 -0300

    new deploy >> aliases, configs, resources, multi-region

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+provider "aws" {
+  alias = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
+}
+
 resource "aws_instance" "app_server" {
   count         = 3
   ami           = "ami-045137e8d34668746"
@@ -45,6 +51,17 @@ resource "aws_instance" "dev5" {
     Name = "dev5"
   }
   vpc_security_group_ids = [ "${aws_security_group.ssh-access.id}" ]
+}
+
+resource "aws_instance" "dev6" {
+  provider      = "aws.us-east-1"
+  ami           = "ami-09cce346b3952cce3"
+  instance_type = "t2.micro"
+  key_name      = "terraform-kali"
+  tags = {
+    Name = "dev6"
+  }
+  vpc_security_group_ids = [ "${aws_security_group.ssh-access-us-east-1.id}" ]
 }
 
 resource "aws_s3_bucket" "dev4" {
--- security-group.tf
+++ security-group.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "ssh-access" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["187.19.208.26/32"]
+    cidr_blocks      = ["187.19.208.98/32"]
     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
@@ -22,5 +22,33 @@ resource "aws_security_group" "ssh-access" {
 
   tags = {
     Name = "ssh-access"
+  }
+}
+
+resource "aws_security_group" "ssh-access-us-east-1" {
+  provider    = "aws.us-east-1"
+  name        = "ssh-access-us-east-1"
+  description = "Allow SSH inbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["187.19.208.98/32"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-access-us-east-1"
   }
 }
